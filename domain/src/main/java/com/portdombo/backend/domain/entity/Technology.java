e860964bebb9816ac8f025f3b7da5a4a3315ac4a
gervasioartur <gervasioartur@outlook.com.br>
2025-04-16 20:27:21 -0300

    feat: add Technology domain entity

---
+++ domain/src/main/java/com/portdombo/backend/domain/entity/Technology.java
@@ -0,0 +1,9 @@
+package com.portdombo.backend.domain.entity;
+
+public class Technology {
+    private Long code;
+    private String name;
+    private String description;
+    private String image;
+    private boolean highlighted;
+}
