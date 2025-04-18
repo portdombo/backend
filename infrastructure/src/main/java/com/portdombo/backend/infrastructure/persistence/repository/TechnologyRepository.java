bb3b27271601bd1af16210f05871d0395bca4835
gervasioartur <gervasioartur@outlook.com.br>
2025-04-18 17:34:36 -0300

    feat: ensure TechnologyService returns false if technology does not exist on exists technology by name

--- infrastructure/src/main/java/com/portdombo/backend/infrastructure/persistence/repository/TechnologyRepository.java
+++ infrastructure/src/main/java/com/portdombo/backend/infrastructure/persistence/repository/TechnologyRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface TechnologyRepository extends JpaRepository<TechnologyEntity, UUID> {
+    boolean existsByName(String name);
 }
