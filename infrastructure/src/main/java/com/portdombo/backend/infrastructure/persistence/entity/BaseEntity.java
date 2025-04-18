75179be980a760e4a7bb74820adaf61458e229ba
gervasioartur <gervasioartur@outlook.com.br>
2025-04-18 17:10:39 -0300

    feat: ensure TechnologyService  saves technology

---
+++ infrastructure/src/main/java/com/portdombo/backend/infrastructure/persistence/entity/BaseEntity.java
@@ -0,0 +1,7 @@
+package com.portdombo.backend.infrastructure.persistence.entity;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
