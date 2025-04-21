bd02adfabc4cde540687283ff6d292b9381d48c4
gervasioartur <gervasioartur@outlook.com.br>
2025-04-21 11:37:13 -0300

    feat: ensure TechnologyController return all technologies on read all success

--- infrastructure/src/main/java/com/portdombo/backend/infrastructure/api/controller/TechnologyController.java
+++ infrastructure/src/main/java/com/portdombo/backend/infrastructure/api/controller/TechnologyController.java
@@ -5,19 +5,18 @@ import com.portdombo.backend.infrastructure.api.dto.CreateTechnologyRequest;
 import com.portdombo.backend.infrastructure.api.dto.Response;
 import com.portdombo.backend.infrastructure.mapper.TechnologyMapper;
 import com.portdombo.backend.usecase.technology.ICreateTechnology;
+import com.portdombo.backend.usecase.technology.IReadAllTechnologies;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -25,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Technology", description = "Technology")
 public class TechnologyController {
     private final ICreateTechnology createTechnology;
+    private final IReadAllTechnologies readAllTechnologies;
     private final TechnologyMapper mapper;
 
     @PostMapping
@@ -40,5 +40,17 @@ public class TechnologyController {
         createTechnology.create(technology);
         Response response = Response.builder().statusCode(HttpStatus.CREATED.value()).data("CREATED").build();
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    @Operation(summary = "Read all Technology")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
+    public ResponseEntity<Response> readAll() {
+        List<Technology> technologies = readAllTechnologies.readAll();
+        Response response = Response.builder().statusCode(HttpStatus.OK.value()).data(technologies).build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
