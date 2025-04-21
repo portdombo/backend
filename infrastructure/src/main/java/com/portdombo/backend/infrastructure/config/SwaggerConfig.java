81de3d01618830ecf445fabc9d09e1b9795389a0
gervasioartur <gervasioartur@outlook.com.br>
2025-04-21 13:36:06 -0300

    chore: add configuration for swagger and handle notfound exceptions

--- infrastructure/src/main/java/com/portdombo/backend/infrastructure/config/SwaggerConfig.java
+++ infrastructure/src/main/java/com/portdombo/backend/infrastructure/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.portdombo.backend.infrastructure.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
