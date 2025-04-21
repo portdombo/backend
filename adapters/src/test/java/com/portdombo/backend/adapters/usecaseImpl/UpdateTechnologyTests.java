010468a0a45efddcfeda2038d7bed9d29e4b84d5
gervasioartur <gervasioartur@outlook.com.br>
2025-04-21 14:23:30 -0300

    test: ensure UpdateTechnology throws ConflictException if the new name has been taken on update

--- adapters/src/test/java/com/portdombo/backend/adapters/usecaseImpl/UpdateTechnologyTests.java
+++ adapters/src/test/java/com/portdombo/backend/adapters/usecaseImpl/UpdateTechnologyTests.java
@@ -4,7 +4,9 @@ import com.portdombo.backend.adapters.IUpdateTechnologyGateway;
 import com.portdombo.backend.adapters.mocks.TechnologyFactory;
 import com.portdombo.backend.adapters.usesaceImpl.UpdateTechnology;
 import com.portdombo.backend.domain.entity.Technology;
+import com.portdombo.backend.domain.exceptions.ConflictException;
 import com.portdombo.backend.domain.exceptions.NotFoundException;
+import com.portdombo.backend.usecase.technology.IExistsTechnologyByName;
 import com.portdombo.backend.usecase.technology.IReadTechnologyByCode;
 import com.portdombo.backend.usecase.technology.IUpdateTechnology;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,12 +21,14 @@ public class UpdateTechnologyTests {
     private IUpdateTechnology updateTechnology;
     private IReadTechnologyByCode readTechnologyByCode;
     private IUpdateTechnologyGateway gateway;
+    private IExistsTechnologyByName existsTechnologyByName;
 
     @BeforeEach
     void setUp() {
         readTechnologyByCode = mock(IReadTechnologyByCode.class);
+        existsTechnologyByName = mock(IExistsTechnologyByName.class);
         gateway =  mock(IUpdateTechnologyGateway.class);
-        updateTechnology = new UpdateTechnology(readTechnologyByCode,gateway);
+        updateTechnology = new UpdateTechnology(readTechnologyByCode,existsTechnologyByName,gateway);
     }
 
 
@@ -37,6 +41,23 @@ public class UpdateTechnologyTests {
         assertThat(exception).isInstanceOf(NotFoundException.class);
         assertThat(exception.getMessage()).isEqualTo("Technology not found");
         verify(readTechnologyByCode, times(1)).read(technology.getCode());
+        verify(gateway, times(0)).update(technology);
+    }
+
+    @Test
+    @DisplayName("Should trow ConflictException if technology name is changed and it already had been taken on update")
+    void shouldTrowConflictExceptionIfTechnologyNameIsChangedAndItAlreadyHadBeenTakenOnUpdate() {
+        Technology technology = TechnologyFactory.createTechnologyFactory();
+        technology.setName("New name");
+        Technology savedTechnology = TechnologyFactory.createTechnologyFactory();
+
+        when(readTechnologyByCode.read(technology.getCode())).thenReturn(savedTechnology);
+        when(existsTechnologyByName.existsByName(technology.getName())).thenReturn(true);
+        Throwable exception = catchThrowable(() -> updateTechnology.update(technology));
+        assertThat(exception).isInstanceOf(ConflictException.class);
+        assertThat(exception.getMessage()).isEqualTo("You already have a technology with this name! Please change the name before update.");
+        verify(readTechnologyByCode, times(1)).read(technology.getCode());
+        verify(existsTechnologyByName, times(1)).existsByName(technology.getName());
         verify(gateway, times(0)).update(technology);
     }
 }
