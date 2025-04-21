163419755934c12016b10248f7e4749c1aac3909
gervasioartur <gervasioartur@outlook.com.br>
2025-04-21 16:35:05 -0300

    test: ensure DeleteTechnology throws NotFoundException if technology does not exist oon delete technology

---
+++ adapters/src/test/java/com/portdombo/backend/adapters/usecaseImpl/DeleteTechnologyTests.java
@@ -0,0 +1,39 @@
+package com.portdombo.backend.adapters.usecaseImpl;
+
+import com.portdombo.backend.adapters.gateway.IDeleteTechnologyGateway;
+import com.portdombo.backend.adapters.usesaceImpl.DeleteTechnology;
+import com.portdombo.backend.domain.exceptions.NotFoundException;
+import com.portdombo.backend.usecase.technology.IDeleteTechnology;
+import com.portdombo.backend.usecase.technology.IReadTechnologyByCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.mockito.Mockito.*;
+
+public class DeleteTechnologyTests {
+    private IDeleteTechnology deleteTechnology;
+    private IReadTechnologyByCode readTechnologyByCode;
+    private IDeleteTechnologyGateway gateway;
+
+    @BeforeEach
+    void setUp() {
+        readTechnologyByCode = mock(IReadTechnologyByCode.class);
+        gateway = mock(IDeleteTechnologyGateway.class);
+        deleteTechnology = new DeleteTechnology(readTechnologyByCode, gateway);
+    }
+
+    @Test
+    @DisplayName("Should throw NotFoundException if technology does not exist")
+    void shouldThrowNotFoundExceptionIfTechnologyDoesNotExist() {
+        Long code =  1L;
+        when(readTechnologyByCode.read(code)).thenThrow(new NotFoundException("Technology not found"));
+        Throwable exception = catchThrowable(() -> deleteTechnology.delete(code));
+        assertThat(exception).isInstanceOf(NotFoundException.class);
+        assertThat(exception.getMessage()).isEqualTo("Technology not found");
+        verify(readTechnologyByCode, times(1)).read(code);
+        verify(gateway, times(0)).delete(code);
+    }
+}
