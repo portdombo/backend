90c77a73fe9185905667b135700f0fc9967db7f1
gervasioartur <gervasioartur@outlook.com.br>
2025-04-21 16:54:40 -0300

    test: ensure TechnologyController returns 404 if Technology does not exist ond delete

--- infrastructure/src/test/java/com/portdombo/backend/infrastructure/api/controller/TechnologyControllerTests.java
+++ infrastructure/src/test/java/com/portdombo/backend/infrastructure/api/controller/TechnologyControllerTests.java
@@ -221,7 +221,7 @@ public class TechnologyControllerTests {
 
     @Test
     @DisplayName("Should return 404 if technology does not exist on update")
-    void shouldReturn404IfTechnologyDoesNotExistOnDelete() {
+    void shouldReturn404IfTechnologyDoesNotExistOnUpdate() {
         Long code = 1L;
         given()
                 .contentType(ContentType.JSON)
@@ -250,5 +250,18 @@ public class TechnologyControllerTests {
                 .patch(BASE_URL + "technologies/" + entity.getCode())
                 .then()
                 .statusCode(204);
+    }
+
+    @Test
+    @DisplayName("Should return 404 if technology does not exist on delete")
+    void shouldReturn404IfTechnologyDoesNotExistOnDelete() {
+        Long code = 1L;
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .delete(BASE_URL + "technologies/" + code)
+                .then()
+                .statusCode(404)
+                .body("data", equalTo("Technology not found"));
     }
 }
